5b1983930a0f95b93e995ff14566f3227d3b8bfe
Biem <Abiemassyikin@gmail.com>
2019-11-01 09:28:16 +0700

    show password in login view

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -8,15 +8,15 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-            
-              <div class="card card-login card-hidden">
-                <div class="card-header card-header-rose text-center">
-                  <h4 class="card-title">Login</h4>
-                  TELKOM INDONESIA
-                </div>
-                <div class="card-body ">
-                  <p class="card-description text-center">Masuk Disini</p>
-                  <form class="form" method="POST" action="<?php echo base_url('Login/aksi_login') ?>">
+
+            <div class="card card-login card-hidden">
+              <div class="card-header card-header-rose text-center">
+                <h4 class="card-title">Login</h4>
+                TELKOM INDONESIA
+              </div>
+              <div class="card-body ">
+                <p class="card-description text-center">Masuk Disini</p>
+                <form class="form" method="POST" action="<?php echo base_url('Login/aksi_login') ?>">
                   <span class="bmd-form-group">
                     <div class="input-group">
                       <div class="input-group-prepend">
@@ -34,7 +34,11 @@
                           <i class="material-icons">lock_outline</i>
                         </span>
                       </div>
-                      <input type="password" class="form-control" required="" placeholder="Password..." name="password">
+                      <input type="password" class="form-control" required="" placeholder="Password..." name="password" id="pass">
+                    </div>
+                    <div style="margin-left: 55px;">
+                      <input type="checkbox" id="show-pass" name="show-pass"/>
+                      <label for="show-pass">Show password</label>
                     </div>
                   </span>
                 </div>
@@ -48,7 +52,35 @@
         </div>
       </div>
       <?php $this->load->view("side/footer"); ?>
+      <script>
+(function() {
+    var _show = function( element, field ) {
+        this.element = element;
+        this.field = field;
+        this.toggle();    
+    };
+    _show.prototype = {
+        toggle: function() {
+            var self = this;
+            self.element.addEventListener( "change", function() {
+                if( self.element.checked ) {
+                    self.field.setAttribute( "type", "text" );
+                } else {
+                    self.field.setAttribute( "type", "password" );    
+                }
+            }, false);
+        }
+    };
+    
+    document.addEventListener( "DOMContentLoaded", function() {
+        var checkbox = document.querySelector( "#show-pass" ),
+            pass = document.querySelector( "#pass" ),
+            _form = document.querySelector( "form" );
+            var toggler = new _show( checkbox, pass );
+    });
+})();
+</script>
       <?php $this->load->view("side/js"); ?>
       <?php if ($this->session->flashdata()) { ?>
-                        <?php echo $this->session->flashdata('Pesan'); ?>                   
-                    <?php } ?>
+        <?php echo $this->session->flashdata('Pesan'); ?>                   
+        <?php } ?>
